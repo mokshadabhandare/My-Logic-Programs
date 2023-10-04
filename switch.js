435488d3fea61d4a2722d6e46b979a9e4f9109c9
mokshada <mokshadabhandare.01@gmail.com>
2023-10-04 11:54:12 +0530

    logic

---
+++ switch.js
@@ -0,0 +1,22 @@
+let std = parseInt(prompt("Enter Your Class"));
+
+switch (std)
+{
+    case 1 :
+        document.write("You are in 1st Standard");
+        break;
+    case 2 :
+        document.write("You are in 2nd Standard");
+        break;   
+    case 3 :
+        document.write("You are in 3rd Standard");
+        break;   
+    case 4 :
+        document.write("You are in 4th Standard");
+        break;
+    case 5 :
+        document.write("You are in 5th Standard");
+        break;  
+    default :
+        document.write("Invalid Standar you Entered");
+}
