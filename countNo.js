ea0cec07ccb80e39bac75e280512023111341cb7
mokshada <mokshadabhandare.01@gmail.com>
2023-10-10 22:23:09 +0530

    logic

---
+++ countNo.js
@@ -0,0 +1,17 @@
+//Count number of digit in number
+
+
+let num=parseInt(prompt("Enter number"));
+let cnt=0;
+
+function countNo(num,cnt)
+{  
+while(cnt<=num)
+{
+    if(num%10==0)
+{
+    document.write(i);
+}
+}
+}
+countNo(num,cnt);
