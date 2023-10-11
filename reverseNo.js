d96178d6dc0961ea97ce9cf98d3d5f05eb16845a
mokshada <mokshadabhandare.01@gmail.com>
2023-10-11 22:04:23 +0530

    logic

---
+++ reverseNo.js
@@ -0,0 +1,16 @@
+// enter number and print it reverse
+
+let num=parseInt(prompt("Enter number"));
+
+function reverse(num)
+{
+    let rem,rev=0;
+    while(num!=0)
+    {
+        rem=num%10;
+        rev=(rev*10)+rem;
+        num=Math.floor(num/10);
+    }
+    document.write("Reversed number"+rev);
+}
+reverse(num);
