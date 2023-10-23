fe699169dc9f45aa518e8a4aa78357c930ae122c
Mokshada Bhandare <139880599+mokshadabhandare@users.noreply.github.com>
2023-10-23 17:11:46 +0530

    MaxNo.js

---
+++ MaxNo.js
@@ -0,0 +1,10 @@
+To find the maximum number in an array using JavaScript, you can use the `Math.max()` function along with the spread operator (`...`) to pass the array elements as individual arguments. Here's an example code snippet:
+
+```javascript
+const numbers = [5, 10, 3, 8, 2];
+const maxNumber = Math.max(...numbers);
+
+console.log("The maximum number is: " + maxNumber);
+```
+
+In this example, we have an array called `numbers` with some values. By using `Math.max()` with the spread operator, we can find the maximum number in the array. The result will be stored in the `maxNumber` variable. Finally, we can display the maximum number using `console.log()`. Give it a try and let me know if you have any questions!
